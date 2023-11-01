2254aef60ff56f697b11066a89c4408f35928bd6
tmub <teemu.manttari@gmail.com>
2023-11-01 11:22:17 +0200

    Initial Commit

---
+++ MUSAP-demo-app-ios/MUSAP_demo_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MUSAP_demo_app_iosApp.swift
+//  MUSAP-demo-app-ios
+//
+//  Created by Teemu Mänttäri on 1.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MUSAP_demo_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
