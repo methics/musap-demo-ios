e5ea7719d9e8e6ce9bb2df03b8d57b82e6509e81
tmub <teemu.manttari@gmail.com>
2024-01-23 12:13:48 +0200

    add readme

--- MUSAP-demo-app-ios/MUSAP_demo_app_ios.swift
+++ MUSAP-demo-app-ios/MUSAP_demo_app_ios.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import musap_ios
 
 @main
 struct MUSAP_demo_app_ios: App {
+    
+    init() {
+        // Enable SSCDs. For example YubikeySscd
+        MusapClient.enableSscd(sscd: YubikeySscd())
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationTabView()
